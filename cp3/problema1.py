1401367fe98f156db1ddad51ce0714c263002497
Mário Umbelino <marioherbert2016@gmail.com>
2023-06-21 20:46:03 -0300

    Calculadora de Paralelogramas
    Calculadora simples de paralelogramas

---
+++ cp3/problema1.py
@@ -0,0 +1,7 @@
+print('-' * 60)
+print(f'{"Bem vindo a Calculadora de Paralelogramas":^60}')
+print('-' * 60)
+comp = float(input('Digite o valor do comprimento da figura: '))
+alt = float(input('Digite o valor da altura da figura: '))
+area = comp * alt
+print(f'A área do paralelograma é: {area:.2f}')
